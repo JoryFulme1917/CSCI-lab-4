e467f093ffa42420931ce7db077035312d66aeee
Mason <mason.dougan16@gmail.com>
2025-03-13 09:12:54 -0500

    Target Script + reset Button script

---
+++ Assets/Scripts/ResetButton.cs
@@ -0,0 +1,52 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ResetButton : MonoBehaviour
+{
+
+
+    private void OnTriggerEnter(Collider collision)
+    {
+
+        Debug.Log("Reset Button Hit");
+
+        GameObject target1 = GameObject.Find("t1");
+        GameObject target2 = GameObject.Find("t2");
+        GameObject target3 = GameObject.Find("t3");
+        GameObject target4 = GameObject.Find("t4");
+        GameObject target5 = GameObject.Find("t5");
+        GameObject target6 = GameObject.Find("t6");
+
+
+        if (collision.gameObject.tag == "Ball")
+        {
+
+            Debug.Log("Ball Hit Reset Button");
+            
+            
+            // reset score counter
+
+            target1.GetComponent<TargetController>().ResetTarget();
+            target2.GetComponent<TargetController>().ResetTarget();
+            target3.GetComponent<TargetController>().ResetTarget();
+            target4.GetComponent<TargetController>().ResetTarget();
+            target5.GetComponent<TargetController>().ResetTarget();
+            target6.GetComponent<TargetController>().ResetTarget();
+
+        }
+    }
+
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/TargetController.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class TargetController : MonoBehaviour
+{
+
+
+
+    private void OnTriggerEnter(Collider collision)
+    {
+
+        Debug.Log("Target Hit");
+
+        if (collision.gameObject.tag == "Ball")
+        {
+            
+            Debug.Log("Ball Hit Target");
+
+            // inc score counter
+
+
+            // play some kind of sound
+
+            // maybe trigger dialogue
+
+            gameObject.SetActive(false);
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
+
+
+
+        }
+
+    }
+
+    public void ResetTarget()
+    {
+        gameObject.SetActive(true);
+        gameObject.GetComponent<MeshRenderer>().enabled = true;
+    }
+
+
+
+
+
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
